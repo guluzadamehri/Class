b6feb1b33501760fd491f8a3721ebba3f318a785
guluzadamehri <guluzadamehri@gmail.com>
2026-04-21 11:07:20 +0400

    Create 65.py

---
+++ Leetcode /65.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s = s.strip()
+        try:
+            if isinstance(float(s),float) or isinstance(int(s),int):
+                return True
+        except Exception as e:
+            return False
