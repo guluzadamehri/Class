9058f606d18a9e6dcdb3908e15c2a8e93b78cf18
guluzadamehri <guluzadamehri@gmail.com>
2026-04-21 11:05:11 +0400

    Add solution for trapping rain water problem
    Implement the trapping rain water algorithm.

---
+++ Leetcode /42.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height:
+        	return 0
+
+        left, right = 0, len(height) - 1
+        leftMax, rightMax = 0, 0
+        result = 0
+        while left < right:
+        	if height[left] < height[right]:
+        		if height[left] > leftMax:
+        			leftMax = height[left]
+        		else:
+        			result += (leftMax - height[left])
+        		left += 1
+        	else:
+        		if height[right] > rightMax:
+        			rightMax = height[right]
+        		else:
+        			result += (rightMax - height[right])
+        		right -= 1
+
+        return result 
